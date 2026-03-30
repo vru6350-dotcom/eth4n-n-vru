20bb24e600952da327bbe7b695f46b2fb503cef0
vru6350-dotcom <vru6350@gmail.com>
2026-03-30 10:08:50 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -609,13 +609,14 @@ client.on('messageCreate', async msg => {
     if (cache.users) {
       if (!cache.users[uid]) cache.users[uid] = { id:uid, username:msg.author.username, coins:0, totalEarned:0, lastDaily:null, inventory:[], redeemedCodes:[] };
       cache.users[uid]._msgCount = (cache.users[uid]._msgCount || 0) + 1;
-if (cache.users[uid]._msgCount % 2 === 0) {
-      cache.users[uid].coins       = (cache.users[uid].coins       || 0) + 1;
-      cache.users[uid].totalEarned = (cache.users[uid].totalEarned || 0) + 1;
+      if (cache.users[uid]._msgCount % 2 === 0) {
+        cache.users[uid].coins       = (cache.users[uid].coins       || 0) + 1;
+        cache.users[uid].totalEarned = (cache.users[uid].totalEarned || 0) + 1;
+      }
       cache.users[uid].username    = msg.author.username;
       scheduleCoinFlush();
     } else {
-      getUser(uid, msg.author.username).then(u => { u.coins+=2; u.totalEarned=(u.totalEarned||0)+2; saveUser(u).catch(()=>{}); }).catch(()=>{});
+      getUser(uid, msg.author.username).then(u => { u._msgCount=(u._msgCount||0)+1; if(u._msgCount%2===0){u.coins+=1;u.totalEarned=(u.totalEarned||0)+1;} saveUser(u).catch(()=>{}); }).catch(()=>{});
     }
   }
 
